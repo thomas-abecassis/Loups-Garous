2b7ab9c980c9f43110719446c120446022654d79
thomas <thomas.abecassis@etu.umontpellier.fr>
2019-11-18 22:59:20 +0200

    quelques commentaires

--- Exemple websocket/server.py
+++ Exemple websocket/server.py
@@ -6,35 +6,37 @@ import websockets
 class WebSocket(object) :
 
 
-
     async def register(self,websocket):
         self.Clients.append(websocket)
 
+    async def unregister(self,websocket):
+        self.Clients.remove(websocket)
 
     async def majChat(self,message):
-        mes=json.dumps({"contenu" : message})
-        await asyncio.wait([client.send(mes) for client in self.Clients])
+        mes=json.dumps({"contenu" : message}) ## on créer un json
+        await asyncio.wait([client.send(mes) for client in self.Clients]) ## qu'on envoie à nos clients
 
-    async def ConnexionWebSocket(self,websocket, path):
+    async def ConnexionWebSocket(self,websocket, path): ## les attributs websocket et path sont donnés automatiquement par la bibliothèque
         
-        # register(websocket) sends user_event() to websocket
-        await self.register(websocket)
+        await self.register(websocket) ## l'objet websocket nous sert à faire la connexion avec un client, c'est avec cet objet 
+        							   ## que l'on recoit(for message in websocket) et envoie(send) des informations
         try:
             async for message in websocket:
-                data = json.loads(message)
+                data = json.loads(message) ## le script js nous envoie un json, c'est avec json.loads que l'on "charge" le json
                 await self.majChat(data["contenu"])
 
         finally:
-            await self.unregister(websocket)
+            await self.unregister(websocket) ## quand le client se déconnecte de la page on prend soint de l'enlever de la liste de client
 
 
     def __init__(self):
-        self.Clients=[]
+        self.Clients=[] ## on initialise la liste où on va stocker les clients qui se connecte
 
-        start_server = websockets.serve(self.ConnexionWebSocket, "localhost", 6790)
+        start_server = websockets.serve(self.ConnexionWebSocket, "localhost", 6790) ## on lance le serveur, cette fonction prend en parametre :
+        																			## une fonction à lancer, l'adresse sur laquelle heberger la connexion et le numéro de port
 
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
 
-test=WebSocket()
+test=WebSocket() ## on créer le websocket (sans cette ligne ca ne marchera pas)
 
